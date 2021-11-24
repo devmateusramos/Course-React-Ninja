247cfd0b67035f6c03811c9d8b2dfa320cb5269d
devmateusramos <devmateusramos@gmail.com>
2021-11-24 15:09:36 -0300

    classes

--- seção 1/src/app.js
+++ seção 1/src/app.js
@@ -1,15 +1,25 @@
 'use strict';
-import React from 'react';
+import React, { Component } from 'react';
 import Title from './title';
 
-var App = React.createClass({
-  render: function () {
+class App extends Component {
+  render() {
     return (
       <div className='container'>
         <Title name='Mateus' />
       </div>
     );
-  },
-});
+  }
+}
+
+// var App = React.createClass({
+//   render: function () {
+//     return (
+//       <div className='container'>
+//         <Title name='Mateus' />
+//       </div>
+//     );
+//   },
+// });
 
 export default App;
