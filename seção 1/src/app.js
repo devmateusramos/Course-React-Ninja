469adcb81cb77030c636c7ec3b3dfd5574e0f1cc
devmateusramos <devmateusramos@gmail.com>
2021-11-24 14:36:31 -0300

    props e sintaxe

--- seção 1/src/app.js
+++ seção 1/src/app.js
@@ -1,9 +1,18 @@
 'use strict';
 import React from 'react';
+import Title from './title';
 
 var App = React.createClass({
   render: function () {
-    return <h1>Título atualizado</h1>;
+    return (
+      <div className='container'>
+        <Title name='Mateus Ramos' />
+        <label htmlFor='input' data-label='Label'>
+          Input
+        </label>
+        <input type='text' id='input' aria-hidden={true}></input>
+      </div>
+    );
   },
 });
 
