c4502a226042e3609929171f1486b9922843c60a
devmateusramos <devmateusramos@gmail.com>
2021-11-27 12:31:07 -0300

    statefull vs stateless

--- seção 1/src/app.js
+++ seção 1/src/app.js
@@ -1,21 +1,25 @@
 'use strict';
 import React, { Component } from 'react';
+import Square from './Square';
+import Button from './button';
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      text: 'Mateus',
+      color: 'green',
     };
   }
 
   render() {
     return (
-      <div
-        className='container'
-        onClick={() => this.setState({ text: 'Outro texto' })}
-      >
-        {this.state.text}
+      <div>
+        <Square color={this.state.color} />
+        {['red', 'green', 'blue'].map((color) => (
+          <Button key={color} handleClick={() => this.setState({ color })}>
+            {color}
+          </Button>
+        ))}
       </div>
     );
   }
