8beff64c52d1c01ebde808f92a9f40c33832cb44
devmateusramos <devmateusramos@gmail.com>
2021-11-27 13:10:04 -0300

    lifecycle mount unmount

--- seção 1/src/app.js
+++ seção 1/src/app.js
@@ -2,24 +2,37 @@
 import React, { Component } from 'react';
 import Square from './Square';
 import Button from './button';
+import Timer from './timer';
 
 class App extends Component {
   constructor() {
+    console.log('constructor');
     super();
     this.state = {
-      color: 'green',
+      showTimer: true,
     };
   }
 
+  componentWillMount() {
+    console.log('componentWillMount');
+  }
+
+  componentDidMount() {
+    console.log('componentDidMount');
+  }
+
   render() {
+    console.log('render');
     return (
       <div>
-        <Square color={this.state.color} />
-        {['red', 'green', 'blue'].map((color) => (
-          <Button key={color} handleClick={() => this.setState({ color })}>
-            {color}
-          </Button>
-        ))}
+        {this.state.showTimer && <Timer />}
+        <button
+          onClick={() => {
+            this.setState({ showTimer: !this.state.showTimer });
+          }}
+        >
+          Show/Hide timer
+        </button>
       </div>
     );
   }
