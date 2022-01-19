a7ae0d74175e94b8ba29b77f1dd2227b6520199d
devmateusramos <devmateusramos@gmail.com>
2022-01-19 17:58:42 -0300

    github app start

---
+++ seção 1/githubApp/src/app.js
@@ -0,0 +1,61 @@
+'use strict';
+import React, { Component } from 'react';
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      checked: false,
+      showContent: false,
+    };
+  }
+
+  render() {
+    return (
+      <div className='app'>
+        <div className='search'>
+          <input
+            type='search'
+            placeholder='Digite o nome do usuário no github'
+          />
+        </div>
+        <div className='user-info'>
+          <img src='https://avatars.githubusercontent.com/u/87035885?v=4' />
+          <h1>
+            <a href='https://github.com/devmateusramos'>Mateus Ramos</a>
+          </h1>
+          <ul className='repos-info'>
+            <li>- Repositórios: 52</li>
+            <li>- Seguidores: 1</li>
+            <li>- Seguindo: 3</li>
+          </ul>
+
+          <div className='actions'>
+            <button>Ver repositórios</button>
+            <button>Ver favoritos</button>
+          </div>
+
+          <div className='repos'>
+            <h2>Repositórios:</h2>
+            <ul>
+              <li>
+                <a href='#'>Nome do repositório</a>
+              </li>
+            </ul>
+          </div>
+
+          <div className='starred'>
+            <h2>Favoritos:</h2>
+            <ul>
+              <li>
+                <a href='#'>Nome do repositório</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
