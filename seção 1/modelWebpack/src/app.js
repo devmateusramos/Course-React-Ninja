e238bc894839bb45e8b8630b31a200bc35483cdf
devmateusramos <devmateusramos@gmail.com>
2022-01-21 16:07:03 -0300

    model ok

---
+++ seção 1/modelWebpack/src/app.js
@@ -0,0 +1,14 @@
+'use strict';
+import React, { Component } from 'react';
+
+class App extends Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    return <div>App</div>;
+  }
+}
+
+export default App;
